d0ce4bcbf63b303cd614fefb35b5906e1481390b
Artem Bachevsky <fryday.kg@gmail.com>
2015-10-22 13:09:28 +0300

    Add src

---
+++ src/settings.py
@@ -0,0 +1,5 @@
+ALLOW_USE_WITHOUT_KEY = False # True or False
+WITHOUT_KEY_EMAIL_TO = 'default@gmail.com' # email address to send if there is not key in arguments
+KEY_EMAILS_MAP = {'fbcf512a5a504ea29b515602678c6607' : 'somemail@gmail.com'} # maps key to email to send
+EMAIL_SUBJECT = 'Info received' # default subject
+EMAIL_FROM = 'admin@gmail.com' # any email address listed in Email API Authorized Senders
